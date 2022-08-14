9e705b21dc70fad422dc98452887d96fb028b2b4
Ashley Richard <ashleymrichard@gmail.com>
2022-08-13 20:36:22 -0700

    re-adds react import

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+import React from "react"
 import { useEffect, useState } from "react"
 import { HashRouter as Routes, Route } from "react-router-dom"
 import Index from "../pages/Index"
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from "react"
 import ReactDOM from "react-dom/client"
 // IMPORT CSS FILE TO BE SOURCE OF STYLING
 import "./index.css"
