8b8c6003f50045cb780a57cd3f50cb347cc00a0d
Ashley Richard <ashleymrichard@gmail.com>
2022-08-13 19:59:34 -0700

    Updates

---
+++ precache-manifest.07c358804a1b056e39a7bb0ee9b7fdc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61cd9a8708e1cfeeba39",
+    "url": "/isitgay/static/css/main.bafce637.chunk.css"
+  },
+  {
+    "revision": "61cd9a8708e1cfeeba39",
+    "url": "/isitgay/static/js/main.61cd9a87.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/isitgay/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "1e32938cfb0182db5360",
+    "url": "/isitgay/static/js/2.1e32938c.chunk.js"
+  },
+  {
+    "revision": "678bdb4ca496a5d58411",
+    "url": "/isitgay/static/js/runtime~main.678bdb4c.js"
+  },
+  {
+    "revision": "87ffda871164f8f65ddb101999f0105a",
+    "url": "/isitgay/index.html"
+  }
+];
