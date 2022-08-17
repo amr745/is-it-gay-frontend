df6798d4bfd002001f0f2fbae51f52a91d0b7399
Ashley Richard <ashleymrichard@gmail.com>
2022-08-17 14:37:40 -0700

    Updates

--- precache-manifest.b49e3893bd966618640740e28b618803.js
+++ precache-manifest.b49e3893bd966618640740e28b618803.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
-    "revision": "af0ce62275bf429436f9",
+    "revision": "38cef105263a35adc68d",
     "url": "/isitgay/static/css/main.91298df1.chunk.css"
   },
   {
-    "revision": "af0ce62275bf429436f9",
-    "url": "/isitgay/static/js/main.af0ce622.chunk.js"
+    "revision": "38cef105263a35adc68d",
+    "url": "/isitgay/static/js/main.38cef105.chunk.js"
   },
   {
     "revision": "2e8f1a6b9f28a667d85d",
     "url": "/isitgay/static/js/1.2e8f1a6b.chunk.js"
   },
   {
-    "revision": "68b5b3bae9b72afb03df",
+    "revision": "1ad4122c28176a6e7930",
     "url": "/isitgay/static/css/2.ac6d74db.chunk.css"
   },
   {
-    "revision": "68b5b3bae9b72afb03df",
-    "url": "/isitgay/static/js/2.68b5b3ba.chunk.js"
+    "revision": "1ad4122c28176a6e7930",
+    "url": "/isitgay/static/js/2.1ad4122c.chunk.js"
   },
   {
     "revision": "c74d65bc1ed6d90f2a2d",
     "url": "/isitgay/static/js/runtime~main.c74d65bc.js"
   },
   {
-    "revision": "23e892738a216d71cd909a55f5710359",
+    "revision": "5f8871c4b037d241453cbb8fe4799462",
     "url": "/isitgay/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/isitgay/precache-manifest.edf95db3f6442fed7c9fc5dda16b241e.js"
+  "/isitgay/precache-manifest.b49e3893bd966618640740e28b618803.js"
 );
 
 workbox.clientsClaim();
